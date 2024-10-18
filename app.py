2bb92c76c4922441e9bb672887236ad4ec258c77
Tacticalim <64941175+Tacticalim@users.noreply.github.com>
2024-10-18 18:14:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import Flask, Markup
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     content = """
-    # Welcome to My Flask Web Page
+    <h1>Welcome to My Flask Web Page</h1>
+    <p>This is a very simple introduction to my web page, served by Flask in Docker.</p>
 
-    This is a very simple introduction to my web page, served by Flask in Docker.
+    <h2>BUT WHY?</h2>
+    <ul>
+        <li>I created this to explore self-hosting a website</li>
+        <li>I also wanted to learn how to use it with Docker.</li>
+        <li>Built this website in a NixOS environment for easy maintenance and propagation across different nodes later on.</li>
+    </ul>
 
-    ## BUT WHY?
-    - I created this to explore a self-hosting a website
-    - I also wanted to learn how to use with Docker. 
-    - Built this website in a NixOS environment for easy maintenance and propogation across different nodes later on.
-
-    ### Glad you're here. Enjoy exploring!
-    #### Maybe i'll put up some advertisements... Hmm...
+    <h3>Glad you're here. Enjoy exploring!</h3>
+    <h4>Maybe I'll put up some advertisements... Hmm...</h4>
     """
-    return Markup(content.replace("\n", "<br>"))
+    return content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
